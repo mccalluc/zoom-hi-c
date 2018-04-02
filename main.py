a4d6c7cb283dcd61afaf794fae193c9f70cbbbb1
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-01 22:00:42 -0400

    Fix aspect ratio

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 from scipy.ndimage import geometric_transform
-from math import sin, cos, pi
+from math import pi
+from cmath import exp
 import png
 
 input_data = [
-    [255, 200, 200, 50, 50, 100, 100],
-    [200, 255, 200, 50, 50, 100, 100],
-    [200, 200, 255, 50, 50, 100, 100],
-    [50, 50, 50, 255, 200, 50, 50],
-    [50, 50, 50, 200, 255, 50, 50],
-    [100, 100, 100, 50, 50, 255, 200],
-    [100, 100, 100, 50, 50, 200, 255]
+    [255, 200, 50, 50, 100, 100],
+    [200, 255, 50, 50, 100, 100],
+    [50, 50, 255, 200, 50, 50],
+    [50, 50, 200, 255, 50, 50],
+    [100, 100, 50, 50, 255, 200],
+    [100, 100, 50, 50, 200, 255]
 ]
 
-def log_unpolar(coord, input_len, output_len):
-    r = -0.5 + coord[0] / output_len
-    theta = (pi / 2) * (coord[1] / output_len)
+def conformal(coord, input_len, output_len):
+    # In the range 0 to 1:
+    x = pi / 2 * coord[0] / output_len
+    y = coord[1] / output_len
+    real = x - 3
+    imag = y * pi / 2
+    mapped = exp(complex(real, imag))
     return (
-        r * sin(theta) * input_len,
-        r * cos(theta) * input_len
+        mapped.real * input_len,
+        mapped.imag * input_len
     )
 
-output_width = 200
-output_height = 400
+output_width = 400
+output_height = 800
 output_data = geometric_transform(
     input=input_data,
-    mapping=log_unpolar,
+    mapping=conformal,
     output_shape=(output_height, output_width),
     order=0,
     extra_keywords={
@@ -33,7 +37,7 @@ output_data = geometric_transform(
     }
 )
 
-f = open('/tmp/warped.png', 'wb')      # binary mode is important
+f = open('/tmp/warped.png', 'wb')
 w = png.Writer(output_width, output_height, greyscale=True)
 w.write(f, output_data)
 f.close()
