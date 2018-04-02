7d4022dfd7bae660f03fcd7d6473f42fab374b54
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-02 11:54:27 -0400

    Read in higlass data

--- main.py
+++ main.py
@@ -2,16 +2,34 @@ from scipy.ndimage import geometric_transform
 from math import pi
 from cmath import exp
 import png
+from base64 import b64decode
+import requests
+from struct import iter_unpack
 
-input_data = [
-    [255, 200, 50, 50, 100, 100, 0],
-    [200, 255, 50, 50, 100, 100, 0],
-    [50, 50, 255, 200, 50, 50, 0],
-    [50, 50, 200, 255, 50, 50, 0],
-    [100, 100, 50, 50, 255, 200, 0],
-    [100, 100, 50, 50, 200, 255, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-]
+
+def hi_c(x, y, z):
+    tile_id = 'CQMd6V_cRw6iCI_-Unl3PQ.{}.{}.{}'.format(x, y, z)
+    params = {
+        'd': tile_id,
+        's': 'AD_Srgd6S1uCsmnFmRJZ3Q'
+    }
+    url = 'http://higlass.io/api/v1/tiles/'
+    tile_data = requests.get(url, params=params).json()[tile_id]
+    dtype = tile_data['dtype']
+    # scale by max_value / min_value
+    if dtype == 'float16':
+        bytes = b64decode(tile_data['dense'])
+        ints = list([i[0] for i in iter_unpack('<H', bytes)])
+        # TODO: Check endian? Check sign?
+        # TODO: Return floats
+        # max_val = tile_data['max_value']
+        # min_val = tile_data['min_value']
+        # span = max_val - min_val
+        # floats = [i[0] * span + min_val for i in ints]
+        # return [floats[i:i + 256] for i in range(0, len(floats), 256)]
+        return [[j/256 for j in ints[i:i + 256]] for i in range(0, len(ints), 256)]
+    else:
+        raise RuntimeError('Unsupported type: ' + dtype)
 
 def conformal(coord, input_len, output_len):
     # In the range 0 to 1:
@@ -25,13 +43,15 @@ def conformal(coord, input_len, output_len):
         mapped.imag * input_len
     )
 
+input_data = hi_c(0,0,0)
+
 output_width = 400
 output_height = 800
 output_data = geometric_transform(
     input=input_data,
     mapping=conformal,
     output_shape=(output_height, output_width),
-    order=0,
+    order=0,  # "1" to smooth
     extra_keywords={
         'input_len':len(input_data),
         'output_len':min(output_width, output_height)
