1fc8ac7dc40828ead5c162cb8bb186e83ffc366d
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-01 20:42:57 -0400

    Checkpoint: 2:1 aspect ratio

--- main.py
+++ main.py
@@ -3,32 +3,37 @@ from math import sin, cos, pi
 import png
 
 input_data = [
-    [1,2,255],
-    [2,255,4],
-    [255,4,5]
+    [255, 200, 200, 50, 50, 100, 100],
+    [200, 255, 200, 50, 50, 100, 100],
+    [200, 200, 255, 50, 50, 100, 100],
+    [50, 50, 50, 255, 200, 50, 50],
+    [50, 50, 50, 200, 255, 50, 50],
+    [100, 100, 100, 50, 50, 255, 200],
+    [100, 100, 100, 50, 50, 200, 255]
 ]
 
 def log_unpolar(coord, input_len, output_len):
-    r = coord[0] / output_len
+    r = -0.5 + coord[0] / output_len
     theta = (pi / 2) * (coord[1] / output_len)
     return (
         r * sin(theta) * input_len,
         r * cos(theta) * input_len
     )
 
-output_len = 200
+output_width = 200
+output_height = 400
 output_data = geometric_transform(
     input=input_data,
     mapping=log_unpolar,
-    output_shape=(output_len, output_len),
+    output_shape=(output_height, output_width),
     order=0,
     extra_keywords={
         'input_len':len(input_data),
-        'output_len':output_len
+        'output_len':min(output_width, output_height)
     }
 )
 
 f = open('/tmp/warped.png', 'wb')      # binary mode is important
-w = png.Writer(output_len, output_len, greyscale=True)
+w = png.Writer(output_width, output_height, greyscale=True)
 w.write(f, output_data)
 f.close()
